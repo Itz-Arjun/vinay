6f3ce1631c0be55c325bb6c4f6290e9a3620573d
Itz-Arjun <arjunrathodrathod48@gmail.com>
2024-11-26 17:43:39 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+def to_uppercase (str1):
+    count=0
+    for letter in str1 [:4]:
+        if letter.str1==letter:
+            count+=1
+        if count>=2:
+            return str1.upper()
+        return str1
+
+str=input("enter a string")
+print(to_uppercase(str))
